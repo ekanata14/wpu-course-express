1277168f6d9d227ceaed692b6ad38b13710ba468
ekanata14 <ekanata1411@gmail.com>
2025-11-04 17:16:11 +0800

    feat: building API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./routes/api";
 
 const app = express();
+app.use(bodyParser.json());
 const PORT = 3000;
 
 app.use("/api", router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on https://localhost:${PORT}`);
+});
+
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to my API",
+        data: "Hello World!"
+    });
 });
